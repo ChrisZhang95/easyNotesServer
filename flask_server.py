d5475bf183b110ad7c230bc59d12c9f5af877a1e
ChrisZhang95 <chris.zhang95@hotmail.com>
2019-01-05 18:22:32 -0500

    Print text can be converted to text

--- flask_server.py
+++ flask_server.py
@@ -1,6 +1,29 @@
 from flask import Flask, jsonify, request
+
+#ocr libraries
+from PIL import Image
+import sys
+import pyocr
+import pyocr.builders
+
 app = Flask(__name__)
 
+#setting up for ocr
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+# The tools are returned in the recommended order of usage
+tool = tools[0]
+print("Will use tool '%s'" % (tool.get_name()))
+# Ex: Will use tool 'libtesseract'
+
+langs = tool.get_available_languages()
+print("Available languages: %s" % ", ".join(langs))
+lang = 'eng'
+print("Will use lang '%s'" % (lang))
+
+#routing
 @app.route('/')
 def hello_world():
     return jsonify({"about": "Hello world!"})
@@ -13,9 +36,15 @@ def get_mult(num):
 def convertImg():
 
     image = request.files["image"]
-    image.save("img.jpeg")
-    return jsonify({'result': 'succeeded!!!'})
-    #return jsonify({'result': 'succeeded!!!'})
+    image.save("img.jpg")
+    txt = tool.image_to_string(
+        Image.open('img.jpg'),
+        lang=lang,
+        builder=pyocr.builders.TextBuilder()
+    )
+
+    return txt
+    #return jsonify({'result': txt})
 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0')
--- readME.txt
+++ readME.txt
@@ -1,1 +1,3 @@
-flask run --host=0.0.0.0  (Run this in command line when start up the server to make it visible across all devices in the local network)
+export FLASK_APP=flask_server.py
+flask run --host=0.0.0.0  
+(Run this in command line when start up the server to make it visible across all devices in the local network)
